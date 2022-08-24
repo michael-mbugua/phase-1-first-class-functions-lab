b44240ab1eda084f80b13a8aacd9eff22f3f511a
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-08-24 18:38:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = n => {
+    let y = n.slice(0,2)
+    return y
+}
+
+const returnLastTwoDrivers = n => {
+    let y = n.slice(-2)
+    return y
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(n){
+    return function(f){
+        return f * n
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler  = createFareMultiplier(3)
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers)
+}
